4c327a6acce76989bf8fd0a16961d6640670a4cd
Gautam Mittal <gautam@mittal.net>
2018-01-31 17:23:18 -0800

    Train real model.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import itertools
 import pandas as pd
 import re
 from util import contractions
 from gensim.models import Word2Vec
 
-data = pd.read_csv('./data/songdata.csv')
+data = pd.read_csv('./data/songdata.csv') # ~2 million sentences
 
 def preprocess(text):
     try:
@@ -17,8 +18,14 @@ def text2Sent(lyrics):
         lambda x: len(x) > 0,
         [preprocess(s.rstrip()) for s in lyrics.split('\n')]))
 
+def buildSentences():
+    dataset = []
+    for song in data['text']:
+        dataset += text2Sent(song)
+    return dataset
+
 print('Building sentences...')
-sentences = [text2Sent(song) for song in data['text']][0]
+sentences = buildSentences()
 print('Training word2vec...')
 model = Word2Vec(sentences, workers=4)
 model.save('./data/model')
