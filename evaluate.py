cb9f0304d166c30bb0dad8df9d070d386d68e9bc
Gautam Mittal <gautam@mittal.net>
2018-02-17 19:50:12 -0800

    Implement song recommendation.

--- evaluate.py
+++ evaluate.py
@@ -1,11 +1,21 @@
-from gensim.models import Word2Vec
-import tfidf
+import numpy as np
+import operator
 import pickle
+import tfidf
+from gensim.models import Word2Vec
 
 model = Word2Vec.load('./data/model')
-# print(model.most_similar('wiz', topn=1))
-
 with open('./data/songvec.pickle', 'rb') as handle:
     songs = pickle.load(handle)
 
-print(songs['Dancing Queen'])
+def cos_sim(u, v):
+    return np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
+
+def nn(v, s):
+    p = {song: cos_sim(v, vec) for song, vec in s.items()}
+    return sorted(p.items(), key=operator.itemgetter(1), reverse=True)
+
+def recommend_songs(song, top_n=10):
+    return nn(songs[song], songs)[:top_n]
+
+print(recommend_songs('Dancing Queen'))
